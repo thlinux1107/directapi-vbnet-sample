891acc239d624f30a8d4b2177c5d14e02d0f111c
Thomas Hagan <thlinux@gmail.com>
2016-11-01 14:09:10 -0400

    Upload Sample Files

---
+++ DirectAPI_VBnet_Sample/DirectAPI_VBnet_Sample/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtMerchantId = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtMerchantKey = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtClientSecret = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtClientId = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtJSONrequest = New System.Windows.Forms.TextBox()
+        Me.btProcess = New System.Windows.Forms.Button()
+        Me.btLoad = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtJSONResponse = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtMerchantId
+        '
+        Me.txtMerchantId.Location = New System.Drawing.Point(38, 51)
+        Me.txtMerchantId.Name = "txtMerchantId"
+        Me.txtMerchantId.Size = New System.Drawing.Size(257, 20)
+        Me.txtMerchantId.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Merchant ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Merchant Key"
+        '
+        'txtMerchantKey
+        '
+        Me.txtMerchantKey.Location = New System.Drawing.Point(38, 96)
+        Me.txtMerchantKey.Name = "txtMerchantKey"
+        Me.txtMerchantKey.Size = New System.Drawing.Size(257, 20)
+        Me.txtMerchantKey.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 205)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Client Secret"
+        '
+        'txtClientSecret
+        '
+        Me.txtClientSecret.Location = New System.Drawing.Point(38, 221)
+        Me.txtClientSecret.Name = "txtClientSecret"
+        Me.txtClientSecret.Size = New System.Drawing.Size(257, 20)
+        Me.txtClientSecret.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 160)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Client ID"
+        '
+        'txtClientId
+        '
+        Me.txtClientId.Location = New System.Drawing.Point(38, 176)
+        Me.txtClientId.Name = "txtClientId"
+        Me.txtClientId.Size = New System.Drawing.Size(257, 20)
+        Me.txtClientId.TabIndex = 4
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(338, 35)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(78, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "JSON Request"
+        '
+        'txtJSONrequest
+        '
+        Me.txtJSONrequest.Location = New System.Drawing.Point(341, 51)
+        Me.txtJSONrequest.Multiline = True
+        Me.txtJSONrequest.Name = "txtJSONrequest"
+        Me.txtJSONrequest.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtJSONrequest.Size = New System.Drawing.Size(263, 350)
+        Me.txtJSONrequest.TabIndex = 8
+        '
+        'btProcess
+        '
+        Me.btProcess.Location = New System.Drawing.Point(220, 275)
+        Me.btProcess.Name = "btProcess"
+        Me.btProcess.Size = New System.Drawing.Size(75, 23)
+        Me.btProcess.TabIndex = 10
+        Me.btProcess.Text = "Process"
+        Me.btProcess.UseVisualStyleBackColor = True
+        '
+        'btLoad
+        '
+        Me.btLoad.Location = New System.Drawing.Point(38, 275)
+        Me.btLoad.Name = "btLoad"
+        Me.btLoad.Size = New System.Drawing.Size(125, 23)
+        Me.btLoad.TabIndex = 11
+        Me.btLoad.Text = "Load Test Credentials"
+        Me.btLoad.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(637, 35)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(86, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "JSON Response"
+        '
+        'txtJSONResponse
+        '
+        Me.txtJSONResponse.Location = New System.Drawing.Point(640, 51)
+        Me.txtJSONResponse.Multiline = True
+        Me.txtJSONResponse.Name = "txtJSONResponse"
+        Me.txtJSONResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtJSONResponse.Size = New System.Drawing.Size(263, 350)
+        Me.txtJSONResponse.TabIndex = 12
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(915, 413)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtJSONResponse)
+        Me.Controls.Add(Me.btLoad)
+        Me.Controls.Add(Me.btProcess)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtJSONrequest)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtClientSecret)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtClientId)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtMerchantKey)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtMerchantId)
+        Me.Name = "Main"
+        Me.Text = "Direct API VBnet Sample"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtMerchantId As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtMerchantKey As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtClientSecret As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtClientId As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtJSONrequest As TextBox
+    Friend WithEvents btProcess As Button
+    Friend WithEvents btLoad As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtJSONResponse As TextBox
+End Class
---
+++ DirectAPI_VBnet_Sample/DirectAPI_VBnet_Sample/Form1.vb
@@ -0,0 +1,148 @@
+﻿Imports Newtonsoft.Json
+Imports System.Security.Cryptography
+Imports System.Text
+Imports System.Net
+Imports System.IO
+
+
+
+Public Class Main
+
+    '=====================================================
+    'Sample Direct API request - VB.net
+    'Thomas Hagan
+    'Integration Consultant
+    'Sage Payment Solutions
+    'October 24th, 2016
+    'Application is intended for instructional use only.
+    'If you have any questions, please feel free to email
+    'the Integration Support team at sdksupport@sage.com
+    'Also, please make sure to register for API credentials
+    'at our developer portal: https://developer.sagepayments.com
+    '======================================================
+
+    Private Sub btProcess_Click(sender As Object, e As EventArgs) Handles btProcess.Click
+
+        'TH - Set the variables
+        Dim merchantId As String = txtMerchantId.Text
+        Dim merchantKey As String = txtMerchantKey.Text
+        Dim clientId As String = txtClientId.Text
+        Dim clientSecret As String = txtClientSecret.Text
+        Dim url_sale As String = "https://api-cert.sagepayments.com/bankcard/v1/charges?type=Sale"
+        'TH - http://requestb.in is a good resource for troubleshooting.
+        'Dim url_sale As String = "http://requestb.in"
+        Dim verb As String = "POST"
+        Dim contentType As String = "application/json"
+        Dim hash_authToken As Byte()
+        Dim hash64_authToken As String
+        Dim strOrderNumber As String = "Invoice " & Date.Now.Millisecond
+        Dim strTotalAmount As String = "7"
+        Dim strCardNumber As String = "5454545454545454"
+        Dim strExpDate As String = "1220"
+
+        'TH - Build request message
+        '!!!!!!!!!!!!!!!!!!!!!>>>IMPORTANT<<<!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        'TH - This is the bare minimum request for a Sale or Authorization
+        'More information may be needed to meet the requirements for a 
+        'qualified transaction. Most card-not-present transactions and transactions
+        'where the card was entered manually will require the addition of the street
+        'address, zip code, and the cvv value to qualify for the best rate.
+        '!!!!!!!!!!!!!!!!!!!!!>>>IMPORTANT<<<!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        Dim strRequest As Object = ""
+        strRequest =
+            "{""Ecommerce"":{""OrderNumber"":""" & strOrderNumber &
+            """,""Amounts"":{""Total"":""" & strTotalAmount &
+            """},""CardData"":{""Number"":""" & strCardNumber &
+            """,""Expiration"":""" & strExpDate &
+            """}}}"
+
+        'TH - Display request message
+        txtJSONrequest.Text = strRequest
+
+        'TH - Building timestamp and nonce. Needs to be epoch in seconds.
+        'TH - I am using the timestamp as my Nonce, but it is best to create a
+        'seperate, unique value.
+        Dim timestamp As Integer = (DateTime.UtcNow - New DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds
+        Dim nonce As String = timestamp
+
+
+
+
+        'TH - Build the Authorization
+        Dim authToken As String = verb & url_sale & strRequest & merchantId & nonce & timestamp
+        Using hmac As HMACSHA512 = New HMACSHA512(Encoding.ASCII.GetBytes(clientSecret))
+            hash_authToken = hmac.ComputeHash(Encoding.ASCII.GetBytes(authToken))
+        End Using
+        hash64_authToken = Convert.ToBase64String(hash_authToken)
+
+        'TH - Initiate a Web Request
+        Dim web_request As HttpWebRequest = CType(HttpWebRequest.Create(url_sale), HttpWebRequest)
+
+        'TH - Set the headers and details
+        Dim web_request_headers As WebHeaderCollection = web_request.Headers
+        Console.WriteLine("Configuring web_request to included headers for Sage Direct API")
+        web_request_headers.Add("clientId: " & clientId)
+        web_request_headers.Add("merchantId: " & merchantId)
+        web_request_headers.Add("merchantKey: " & merchantKey)
+        web_request_headers.Add("nonce: " & nonce)
+        web_request_headers.Add("timestamp: " & timestamp)
+        web_request_headers.Add("authorization: " & hash64_authToken)
+        web_request.Method = verb
+
+        'TH - Format the request
+        Dim byteArray As Byte() = Encoding.ASCII.GetBytes(strRequest)
+
+        'TH - Send the data
+        web_request.ContentType = contentType
+        web_request.ContentLength = byteArray.Length
+        Dim datastream As Stream = web_request.GetRequestStream()
+        datastream.Write(byteArray, 0, byteArray.Length)
+        datastream.Close()
+
+        'TH - Get the Response
+        Try
+            Dim web_response As WebResponse = web_request.GetResponse()
+            Console.WriteLine(CType(web_response, HttpWebResponse).StatusDescription)
+            datastream = web_response.GetResponseStream()
+
+
+            Dim reader As New StreamReader(datastream)
+            Dim responseFromServer As String = reader.ReadToEnd()
+            Console.WriteLine(responseFromServer)
+            reader.Close()
+            datastream.Close()
+            web_response.Close()
+
+            'TH - Display response.
+            txtJSONResponse.Text = responseFromServer
+
+            'TH - Catch any errors
+        Catch ex As Exception
+            txtJSONResponse.Text = "Server Response: " & ex.Message
+        End Try
+
+
+
+    End Sub
+
+    Private Sub btLoad_Click(sender As Object, e As EventArgs) Handles btLoad.Click
+
+        'TH - Test Data. This is the test account infomation we provide
+        'in the API Sandbox. Please contact us at sdksupport@sage.com if
+        'you need a unique test account and receive the Merchant ID and
+        'Merchant Key. In order to get your own Client ID and Client Secret
+        'you must register at https://developer.sagepayments.com and setup
+        'an App under My Apps. Please let us know if you have any questions.
+        Dim merchantId As String = "173859436515"
+        Dim merchantKey As String = "P1J2V8P2Q3D8"
+        Dim clientId As String = "W8yvKQ5XbvAn7dUDJeAnaWCEwA4yXEgd"
+        Dim clientSecret As String = "iLzODV5AUsCGWGkr"
+
+        txtMerchantId.Text = merchantId
+        txtMerchantKey.Text = merchantKey
+        txtClientId.Text = clientId
+        txtClientSecret.Text = clientSecret
+
+    End Sub
+End Class
+
